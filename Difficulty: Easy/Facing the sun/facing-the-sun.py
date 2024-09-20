f1554f6422b93a063d0088d3c097d88ad2b7a417
Aditya <130051887+Adi030609@users.noreply.github.com>
2024-09-20 10:26:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Facing the sun/facing-the-sun.py
@@ -0,0 +1,26 @@
+#User function Template for python3
+
+class Solution:
+    # Returns count buildings that can see sunlight
+    def countBuildings(self, height):
+        count = 1
+        mheight=height[0]
+        for i in range(1,len(height)):
+            if height[i]>mheight:
+                count += 1
+                mheight = height[i]
+        return count
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == "__main__":
+    t = int(input().strip())
+    for _ in range(t):
+        height = list(map(int, input().strip().split()))
+        ob = Solution()
+        ans = ob.countBuildings(height)
+        print(ans)
+
+# } Driver Code Ends
