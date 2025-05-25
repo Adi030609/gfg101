c43a186d59efef135dd9f6c7ddf768bd9fbaef21
Aditya <130051887+Adi030609@users.noreply.github.com>
2025-05-25 10:58:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Nth Fibonacci Number/nth-fibonacci-number.py
+++ Difficulty: Easy/Nth Fibonacci Number/nth-fibonacci-number.py
@@ -1,15 +1,13 @@
 
 class Solution:
     def nthFibonacci(self, n: int) -> int:
-        memo={}
-        def fib(n):
-            if n<=1:
-                return n
-            if n in memo:
-                return memo[n]
-            memo[n]=fib(n-1)+fib(n-2)
-            return memo[n]
-        return fib(n)
+        if n<=1:
+            return n
+        dp=[0]*(n+1)
+        dp[1]=1
+        for i in range(2,n+1):
+            dp[i]=dp[i-1]+dp[i-2]
+        return dp[n]
             
         
 
