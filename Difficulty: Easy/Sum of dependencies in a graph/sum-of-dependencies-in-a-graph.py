087fbe840a76a97ac4c0933c912df8c012012380
Aditya <130051887+Adi030609@users.noreply.github.com>
2025-06-30 12:59:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sum of dependencies in a graph/sum-of-dependencies-in-a-graph.py
@@ -0,0 +1,7 @@
+#Back-end complete function Template for Python 3
+
+class Solution:
+    
+    #Function to calculate the sum of dependencies for all vertices.
+    def sumOfDependencies(self,V,edges):
+        return len(edges)
